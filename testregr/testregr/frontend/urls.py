d46e3517f17e86810728c27515257315f43af701
AidenCohen31 <37197645+AidenCohen31@users.noreply.github.com>
2021-08-26 13:46:19 -0500

    Delete testregr/testregr directory

--- testregr/testregr/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,re_path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('loadview',views.lookup),
-    path('getsubtotals',views.lookup_subtotals),
-]
